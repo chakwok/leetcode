d61457ef7a510a026d2019ef38e1b3e442088c1f
akwok <andyk2014@gmail.com>
2019-07-02 23:50:34 +0800

    optimize 2m:

--- src/medium/Q173BinarySearchTreeIterator.java
+++ src/medium/Q173BinarySearchTreeIterator.java
@@ -32,9 +32,40 @@ public class Q173BinarySearchTreeIterator {
     }
 }
 
+//BST
 /**
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator obj = new BSTIterator(root);
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
+
+
+/*
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    */
+/** @return whether we have a next smallest number *//*
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    */
+/** @return the next smallest number *//*
+
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+
+    private void pushAll(TreeNode node) {
+        for (; node != null; stack.push(node), node = node.left);
+    }
+}*/
--- src/medium/Q318MaximumProductofWordLengths.java
+++ src/medium/Q318MaximumProductofWordLengths.java
@@ -10,6 +10,29 @@ import static java.util.Comparator.comparingInt;
 import static java.util.Comparator.reverseOrder;
 
 public class Q318MaximumProductofWordLengths {
+    //4ms
+    public static int maxProductBetter(String[] words) {
+        if (words == null || words.length == 0)
+            return 0;
+        int len = words.length;
+        int[] value = new int[len];
+        for (int i = 0; i < len; i++) {
+            String tmp = words[i];
+            value[i] = 0;
+            for (int j = 0; j < tmp.length(); j++) {
+                value[i] |= 1 << (tmp.charAt(j) - 'a');
+            }
+        }
+        int maxProduct = 0;
+        for (int i = 0; i < len; i++)
+            for (int j = i + 1; j < len; j++) {
+                if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
+                    maxProduct = words[i].length() * words[j].length();
+            }
+        return maxProduct;
+    }
+
+    //80ms. O(nm + n^2)
     public static int maxProduct(String[] words) {
         if(words == null || words.length < 2) {
             return 0 ;
