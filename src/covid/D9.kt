83c32dd6d6f8cb8f3579a69ae18c6ffbdc73c987
akwok <andyk2014@gmail.com>
2020-04-09 19:03:24 +0800

    covid 1q

---
+++ src/covid/D9.kt
@@ -0,0 +1,59 @@
+package covid
+
+class D9 {
+    fun backspaceCompare(S: String, T: String): Boolean {
+        val sBSS = BackSpaceString(S);
+        val tBSS = BackSpaceString(T);
+
+        var sNext = sBSS.next();
+        var tNext = tBSS.next();
+
+        while(sNext != null || tNext != null) {
+            if(sNext != tNext) {
+                return false;
+            }
+
+            sNext = sBSS.next();
+            tNext = tBSS.next();
+        }
+
+        return true;
+    }
+
+    private class BackSpaceString(val str: String) {
+        var pos = str.length - 1;
+        var skip = 0;
+
+        fun next(): Char? {
+            var res : Char? = null;
+
+            while(pos >= 0) {
+                if(str[pos] == '#') {
+                    skip++;
+                    pos--;
+                } else {
+                    if(skip > 0) {
+                        skip--;
+                        pos--;
+                    } else {
+                        res = str[pos];
+                        pos--;
+                        break;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+
+
+}
+
+fun main() {
+    println(D9().backspaceCompare("ab#c", "ad#c"))
+    println(D9().backspaceCompare("a##c", "#a#c"))
+    println(D9().backspaceCompare("a#c", "b"))
+    println(D9().backspaceCompare("bwj##tw", "bwj###tw"))
+
+}
